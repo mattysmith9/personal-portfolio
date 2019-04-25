272b348f1a028bfff76aaa24a00f796bcccce26c
Matty Smith <matthewcsmith18@gmail.com>
2019-04-24 22:45:45 -0400

    ewdeferfef

--- src/components/Nav/NavLinks.js
+++ src/components/Nav/NavLinks.js
@@ -24,14 +24,38 @@ const Wrapper = styled.div`
     color: ${colors.orange};
     border-bottom: 1px solid ${colors.blue};
   }
+  ${({ desktop }) =>
+    desktop
+      ? `
+			@media (max-width: 960px) {
+					display: none;
+			}
+			a {
+					margin-right: 1rem;
+					&:last-child {
+							margin-right: unset;
+					}
+			}
+		`
+      : `
+			padding: 3rem;
+			display: flex;
+			flex-direction: column;
+			a {
+					margin-bottom: 1rem;
+					&:last-child {
+							margin-bottom: unset;
+					}
+			}
+	`}
 `;
 
 const NavLinks = ({ desktop }) => (
   <Wrapper desktop={desktop}>
-    <AnchorLink href="#about">.about</AnchorLink>
-    <AnchorLink href="#projects">.projects</AnchorLink>
-    <AnchorLink href="#skills">.skills</AnchorLink>
-    <AnchorLink href="#contact">.contact</AnchorLink>
+    <AnchorLink href="#about">about</AnchorLink>
+    <AnchorLink href="#projects">projects</AnchorLink>
+    <AnchorLink href="#skills">skills</AnchorLink>
+    <AnchorLink href="#contact">contact</AnchorLink>
   </Wrapper>
 );
 
